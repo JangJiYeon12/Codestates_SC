dca192828c2df6a18f135af540b8b33d4e6d79d7
JangJiYeon12 <1021jja@naver.com>
2022-04-08 11:51:07 +0900

    first

--- src/Part_2.py
+++ src/Part_2.py
@@ -14,7 +14,10 @@ def get_results():
     # Hint: 다음의 링크를 들어가면 body에 들어가는 정보를 확인 할 수 있습니다
     # https://api.openaq.org/v1/measurements?city=Los%20Angeles&parameter=pm25
 
-    results = []
+    date = body.get("date")
+    value = body.get("value")
+
+    results = dict(date, value)
 
     # TODO results 리스트를 리턴합니다.
     return results
@@ -27,11 +30,19 @@ from pymongo import MongoClient
 
 # 자신의 MongoDB 연결정보를 입력합니다.
 # COLLECTION_NAME 은 유지합니다.
-HOST = ''
-USER = ''
-PASSWORD = ''
-DATABASE_NAME = ''
+HOST = 'cluster0.jpu3q.mongodb.net'
+USER = 'Jang-Ji-Yeon'
+PASSWORD = 'wldus1021'
+DATABASE_NAME = 'myFirstDatabase'
 COLLECTION_NAME = 'OpenAQ'
 MONGO_URI = f"mongodb+srv://{USER}:{PASSWORD}@{HOST}/{DATABASE_NAME}?retryWrites=true&w=majority"
+
+client = MongoClient(MONGO_URI)
+
+database = client[DATABASE_NAME]
+collection = database[COLLECTION_NAME]
+
+insert = get_results()
+collection.insert_one(insert)
 
 # MongoDB 에 OpenAQ 에서 받은 body가 입력되어 있어야합니다.
---
+++ src/sksk.py
@@ -0,0 +1,10 @@
+from Part_1 import Product
+
+prod = Product('Merog')
+
+
+from Part_1 import Tea
+
+tea = Tea('Green Tea')
+
+print(tea.calory())
